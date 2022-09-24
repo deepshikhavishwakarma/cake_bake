be416885c350e114020a518ec0f293d49a22ad84
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2022-09-24 01:58:47 -0400

    Initial commit

---
+++ cake web/day1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum{
+	public static void main(String []args){
+		age = 20;
+		if(age>18){
+			System.out.println("Adult");
+
+		}
+		else{
+			System.out.println("child");
+		}
+	}
+}
